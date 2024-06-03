d4b30f8d5ed1569cd8ed6197ba4c28b242112af4
Fredy <fredyultima2@gmail.com>
2024-06-02 22:01:27 -0300

    change NotificationError

--- src/main/java/dev/fredyhg/cleanarchitecture/domain/shared/notification/NotificationError.java
+++ src/main/java/dev/fredyhg/cleanarchitecture/domain/shared/notification/NotificationError.java
@@ -1,8 +1,10 @@
 package dev.fredyhg.cleanarchitecture.domain.shared.notification;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class NotificationError {
     private String message;
     private String context;
